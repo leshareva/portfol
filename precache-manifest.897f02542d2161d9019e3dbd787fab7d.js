24fa5711176476973d685911233a81b125c63f86
leshareva <leshareva.box@gmail.com>
2020-08-16 15:56:28 +0700

    Updates

---
+++ precache-manifest.897f02542d2161d9019e3dbd787fab7d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "327201a9c24bf8b106413a9a360e958c",
+    "url": "/portfol/index.html"
+  },
+  {
+    "revision": "289dc81f1959a2e9d737",
+    "url": "/portfol/static/css/2.1e562f28.chunk.css"
+  },
+  {
+    "revision": "098205d93b77ec3bfef8",
+    "url": "/portfol/static/css/main.b16e5276.chunk.css"
+  },
+  {
+    "revision": "289dc81f1959a2e9d737",
+    "url": "/portfol/static/js/2.bd2da665.chunk.js"
+  },
+  {
+    "revision": "05c6f1a7df6851604df762baf269aac6",
+    "url": "/portfol/static/js/2.bd2da665.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "098205d93b77ec3bfef8",
+    "url": "/portfol/static/js/main.8324812c.chunk.js"
+  },
+  {
+    "revision": "f8e0d5675b7ae66e185a",
+    "url": "/portfol/static/js/runtime-main.6eba4eb9.js"
+  },
+  {
+    "revision": "4af45f9a74a5c77b229323f8a7f6d77d",
+    "url": "/portfol/static/media/close.4af45f9a.svg"
+  },
+  {
+    "revision": "8b6fb32ff8667219ff6f0db4b7e6063b",
+    "url": "/portfol/static/media/cold.8b6fb32f.svg"
+  },
+  {
+    "revision": "525301c7f6cfad7bd94032a5e371132e",
+    "url": "/portfol/static/media/corner.525301c7.svg"
+  },
+  {
+    "revision": "db3a49aad1431c8ea03c59cbd7ab2c72",
+    "url": "/portfol/static/media/hot.db3a49aa.svg"
+  },
+  {
+    "revision": "9830cfc2015e09045b9db1c8e6ec585d",
+    "url": "/portfol/static/media/plus.9830cfc2.svg"
+  },
+  {
+    "revision": "853a71692596f8a9f168a24b99b31402",
+    "url": "/portfol/static/media/wait.853a7169.svg"
+  }
+]);
